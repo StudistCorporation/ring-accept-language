48680c006ff43c514d1c51d1c5e77e1f356ec888
katsuyasu-murata <katsuyasu.murata@studist.jp>
2019-04-26 19:15:34 +0900

    Fix project settings

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject ring/ring-accept "0.1.0-SNAPSHOT"
+(defproject jp.studist/ring-accept-language "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
