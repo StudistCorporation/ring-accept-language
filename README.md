7c88694bbff2e7e11ba3c76a93fc432fd1de2744
katsuyasu-murata <katsuyasu.murata@studist.jp>
2019-05-05 15:17:19 +0900

    Fix README

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# Ring-Accept
+Ring-Accept-language
+====
 
-A Clojure library designed to ... well, that part is up to you.
+## Overview
+
+Ring-Accept-language provides a middleware that sets a hash that convert an accept-language in a request header.
 
 ## Usage
 
-FIXME
+Add a middleware after adding dependencies.
+
+    (defn some-middleware
+      [handler]
+      (wrap-accept-language handler))
 
 ## License
 
-Copyright © 2014 James Reeves
+Copyright © 2019 Studist Corporation
 
 Distributed under the MIT License, the same as Ring.
--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject jp.studist/ring-accept-language "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ring-Accept-language provides a middleware that sets a hash that convert an accept-language in a request header."
+  :url "https://github.com/StudistCorporation/ring-accept-language"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles
-  {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+  :dependencies [[org.clojure/clojure "1.10.0"]])
