32fc562d8dd405238611c7b6e73516f177ef0623
katsuyasu-murata <katsuyasu.murata@studist.jp>
2019-05-07 11:39:05 +0900

    Fix description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jp.studist/ring-accept-language "0.1.0-SNAPSHOT"
-  :description "Ring-Accept-language provides a middleware that sets a hash that convert an accept-language in a request header."
+  :description "Ring-Accept-language provides a middleware that parses the Accept-language request header and injects the value into the request hash."
   :url "https://github.com/StudistCorporation/ring-accept-language"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
